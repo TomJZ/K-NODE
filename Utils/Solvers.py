a4b14dff2b7c1f7a5afaaa84269f258222683b51
TomJZ <zjh@seas.upenn.edu>
2021-11-10 10:48:33 -0500

    added NODE module

---
+++ Utils/Solvers.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+"""## solvers"""
+
+def RK(z0, n_steps, f, h):
+    '''
+    4th Order Runge Kutta Numerical Solver
+    Input:
+      z0: initial condition
+      t0: initial time (not actual time, but the index of time)
+      n_steps: the number of steps to integrate
+      f: vector field
+      h: step size
+    Return:
+      z: the state after n_steps
+    '''
+    z = z0
+    for i in range(int(n_steps)):
+        k1 = h * f(z) 
+        k2 = h * f(z + 0.5 * k1) 
+        k3 = h * f(z + 0.5 * k2) 
+        k4 = h * f(z + k3)
+  
+        z = z + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4)
+    return z
+
+
+def Euler(z0, n_steps, f, step_size):
+    '''
+    Simplest Euler ODE initial value solver
+    Input:
+      z0: initial condition
+      t0: initial time (not actual time, but the index of time)
+      n_steps: the number of steps to integrate
+      f: vector field
+      h: step size
+    Return:
+      z: the state after n_steps
+    '''
+    z = z0
+    for i_step in range(int(n_steps)):
+        z = z + step_size * f(z)
+    return z
